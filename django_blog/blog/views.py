59ac1bd2a3021ae368b519666ce3c7d61d3994e4
NguyenHuuDangKhoa <nhdkhoa1994@gmail.com>
2022-11-01 13:54:46 -0600

    Created the Post model and related database for the Blog app

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Khoa',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': '2022'
-    },
-    {
-        'author': 'Dang',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': '2023'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
 
